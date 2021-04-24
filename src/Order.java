98eb1e42fd000dbb383938b1820c9ca84ca9affc
ahmed <ahmedazab1235@yahoo.com>
2021-04-24 22:04:21 +0200

    add Order java file

---
+++ src/Order.java
@@ -0,0 +1,8 @@
+public class Order {
+    private int orderid ;
+    private string dateCreated ;
+    private int numOfitems ;
+    private float totalprice ;
+    private float tax ;
+    private int items_name ;
+}
