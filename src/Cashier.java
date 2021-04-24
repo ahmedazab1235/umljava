71b3d68e3987afa31e377123c528226877a75976
ahmed <ahmedazab1235@yahoo.com>
2021-04-24 22:02:21 +0200

    add cashier java file

---
+++ src/Cashier.java
@@ -0,0 +1,48 @@
+public class Cashier {
+    private string cashiername  ;
+    private int cashierPhoneNumber ;
+    private int cashieradress  ;
+
+    private void scan_item() {
+
+    }
+    private void display_iteminfo() {
+
+        System.out.println("");
+
+    }
+    private void display_total_iteminfo() {
+
+        System.out.println("total");
+
+    }
+
+    private int get_itemprice() {
+
+        return itemprice ;
+
+    }
+
+    private void itemadd(){
+
+    }
+
+    private void itemremove(){
+
+    }
+
+    private int totalprice() {
+
+        return totalprice ;
+
+    }
+
+    private int tax() {
+
+        return tax ;
+
+    }
+
+
+
+}
