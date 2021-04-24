9aba3d932b31feb5ad596a1f92b820c123315f4f
ahmed <ahmedazab1235@yahoo.com>
2021-04-24 22:06:18 +0200

    add Customer java file

---
+++ src/Customer.java
@@ -0,0 +1,17 @@
+public class Customer {
+    public Customer(int customerid, string customerName,
+                   int customerphoneNumber, string paymentType)
+    {
+        this.customerid = customerid ;
+        this.customerName = customerName ;
+        this.customerphoneNumber = customerphoneNumber;
+        this.paymentType = paymentType;
+
+    }
+
+    public void choosePaytype(){
+
+    }
+
+
+}
---
+++ src/System.java
@@ -0,0 +1,11 @@
+public class System {
+    private string cashierName;
+    private string dateOfstart;
+
+    public void generatesAreceipt(){
+
+    }
+    public void updateGoods(){
+
+    }
+}
